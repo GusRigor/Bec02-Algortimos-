d6e74b0bcdca298328594a53f7b116c91d705597
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2018-09-28 22:35:33 -0300

    Create Ex1.c

---
+++ 28.09/Ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+int matriz[2][3] = {{1,2,3},
+                    {4,5,6}};
+for (int i = 0; i < 3; i++){
+ for (int j = 0; j < 2; j++){
+ printf("%d ", matriz[j][i]);
+ }
+ printf("\n");
+}
+}

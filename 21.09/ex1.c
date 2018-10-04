beecdaf7c9cd8a7544ce1421eb23f240b0a1ef3d
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2018-10-04 11:51:45 -0300

    Update and rename ex1-21.09.c to 21.09/ex1.c

--- 21.09/ex1.c
+++ 21.09/ex1.c
@@ -2,7 +2,8 @@
 int main(){
 int *p, vetor[5] = {1,2,3,4,5};
     p = (vetor+2);
-    (*p)++; printf("%d\n", *p);
+    (*p)++;
+    printf("%d\n", *p);
     printf("%d\n", p[0]);
     printf("%d\n", p[-1]);
     printf("%d\n", p[-2]);

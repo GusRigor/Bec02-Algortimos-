add48c8d3588ae9f89799e2a57bf85eca5690d28
Gustavo Rigor <37718815+GusRigor@users.noreply.github.com>
2018-11-09 20:21:29 -0200

    Create Ex13.c

---
+++ 09.11/Ex13.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.io>
+
+int Invert_Vet(int A[], int n){
+    int i, j, troca;
+
+    for(i=0; i < n-1; ++i){
+        for(j=(i+1); j < n; ++j){
+            if(A[i] < A[j]){
+                temp = A[i];
+                A[i] = A[j];
+                A[j] = temp;
+            }
+        }
+    }
+}
